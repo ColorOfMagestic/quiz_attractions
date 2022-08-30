1338a08dd23f68899ff93acb8af42aeed771994c
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-08-30 18:23:25 +0500

    Basic files on the project

---
+++ script.js
@@ -0,0 +1,19 @@
+// sections
+
+// buttons
+
+// elements
+
+// counts
+
+// Переключение экранов
+
+// Вспомогательные функции
+
+function addClass(v, cls) {
+  v.classList.add(`${cls}`);
+}
+
+function removeClass(v, cls) {
+  v.classList.remove(`${cls}`);
+}
